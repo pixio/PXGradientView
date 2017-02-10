ee1e9844b3491ea150a2d6a570f666d479d5f69a
Michael Biggs <mbiggs@pixio.com>
2017-02-10 15:05:29 -0700

    Added nullability specifiers

--- Pod/Classes/PXGradientView.h
+++ Pod/Classes/PXGradientView.h
@@ -76,7 +76,7 @@ typedef NS_ENUM(NSInteger, PXGradientDirection)
 /**
  *  The colors to use in the gradient.  This is simply an array of UIColors.
  */
-@property (nonatomic) NSArray * gradientColors;
+@property (nonatomic, nullable) NSArray * gradientColors;
 
 /**
  *  The direction the gradient gradates.
@@ -86,7 +86,7 @@ typedef NS_ENUM(NSInteger, PXGradientDirection)
 /**
  *  The points to use along the gradient.  If this property is nil, the colors are evenly spaced across the gradient.
  */
-@property (nonatomic) NSArray * locations;
+@property (nonatomic, nullable) NSArray * locations;
 
 /**
  *  Make a gradient view with a specified default gradient.  This creates a gradient without having to set the
@@ -96,7 +96,7 @@ typedef NS_ENUM(NSInteger, PXGradientDirection)
  *
  *  @return a pre-initialized gradient view
  */
-- (instancetype)initWithDefaultGradient:(PXDefaultGradient)defaultGradient;
+- (nonnull instancetype)initWithDefaultGradient:(PXDefaultGradient)defaultGradient;
 
 /**
  *  Reset the gradient color and location arrays to empty.
